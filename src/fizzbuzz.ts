24a8c95945844039f6872e813d250e50da9b9e53
Elliot Iddon <elliot@land.tech>
2022-06-17 11:38:00 +0100

    Various implementations of fizzbuzz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,45 @@
 const fizzbuzz = (i: number): string => {
-    return "";
+    let answer = ""
+
+    if (i % 3 === 0) {
+        answer += "fizz";
+    }
+    if (i % 5 === 0) {
+        answer += " buzz";
+    }
+
+    return answer.trim() || i.toString(); 
+}
+
+export type Sub = {
+    multiple: number,
+    replacement: string,
+}
+
+export type Spec = {
+    subs: Sub[]
+}
+
+export type CoolSpec = {
+    subs: ((i: number) => string|undefined)[]
+}
+
+export const configFizzBuzz = (i: number, spec: Spec): string => {
+    let answer = []
+
+    spec.subs.forEach(({multiple, replacement}) => {
+        if (i % multiple === 0) {
+            answer.push(replacement)
+        }
+    });
+
+    return answer.join(" ") || i.toString();
+}
+
+export const coolFizzBuzz = (i: number, spec: CoolSpec): string => {
+    return spec.subs.map(x => x(i))
+        .join(" ")
+        .trim() || i.toString();
 }
 
 export default fizzbuzz
--- test/fizzbuzz.spec.ts
+++ test/fizzbuzz.spec.ts
@@ -1,14 +1,43 @@
-import fizzbuzz from "../src/fizzbuzz";
+import fizzbuzz, {configFizzBuzz, CoolSpec, coolFizzBuzz, Sub, Spec} from "../src/fizzbuzz";
 
 describe("Example Test Suite", () => {
-    it("Does something", () => {
-        expect(fizzbuzz(1)).toBe("")
+
+    const coolSpec : CoolSpec = {
+        subs: [
+            (i) => i % 3 === 0 ? 'fizz' : undefined,
+            (i) => i % 5 === 0 ? 'buzz' : undefined,
+        ]
+    }
+    test.each([
+        { i: 1, expected: "1" },
+        { i: 2, expected: "2" },
+        { i: 3, expected: "fizz" },
+        { i: 5, expected: "buzz" },
+        { i: 15, expected: "fizz buzz" },
+    ])('$i = $expected', ({ i, expected }) => {
+        expect(coolFizzBuzz(i, coolSpec)).toBe(expected);
     })
 
-    test.each([
-        {a: 1, b: 2, expected: 3},
-        {a: 5, b: 2, expected: 7},
-    ])('$a + $b = $expected', ({a, b, expected}) => {
-        expect(a + b).toBe(expected)
-    })
+    // const classicSpec : Spec = {
+    //     subs: [
+    //         {multiple: 3, replacement: "fizz"},
+    //         {multiple: 5, replacement: "buzz"}
+    //     ]
+    // }
+    // test.each([
+    //     { i: 1, expected: "1" },
+    //     { i: 2, expected: "2" },
+    //     { i: 3, expected: "fizz" },
+    //     { i: 5, expected: "buzz" },
+    //     { i: 15, expected: "fizz buzz" },        
+    // ])('$i = $expected', ({ i, expected }) => {
+    //     expect(configFizzBuzz(i, classicSpec)).toBe(expected);
+    // })
+
+    // test.each([
+    //     {a: 1, b: 2, expected: 3},
+    //     {a: 5, b: 2, expected: 7},
+    // ])('$a + $b = $expected', ({a, b, expected}) => {
+    //     expect(a + b).toBe(expected)
+    // })
 })
